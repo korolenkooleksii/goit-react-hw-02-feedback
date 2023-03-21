a488482c13cac116fd0c2d9260b8ac56ff0e8ab6
Oleksii Korolenko <as_vostok@i.ua>
2023-03-21 16:26:05 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { App } from './components/App';
+import { ThemeProvider } from '@emotion/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = {
+  colors: {
+    black: 'black',
+    white: 'white',
+    accent: 'tomato',
+  },
+};
+console.log(theme);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
