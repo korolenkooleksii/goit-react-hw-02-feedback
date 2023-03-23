2863837a6227806b93aeecbd2accf85c2eb2a9cb
Oleksii Korolenko <as_vostok@i.ua>
2023-03-23 08:54:48 +0200

    add Object.freez

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,24 +2,25 @@ import { ListButtons, Button, WrapButton } from './FeedbackOptions.styled';
 import { BsHandThumbsUp, BsHandThumbsDown } from 'react-icons/bs';
 import { FaRegHandLizard } from 'react-icons/fa';
 import PropTypes from "prop-types";
+import { variantFeedbacks } from 'components/constans';
 
 const FeedbackOptions = ({ onLeaveFeedback }) => {
   return (
     <ListButtons>
       <WrapButton>
-        <Button type="button" onClick={() => onLeaveFeedback('good')}>
+        <Button type="button" onClick={() => onLeaveFeedback(variantFeedbacks.good)}>
           <BsHandThumbsUp />
           Good
         </Button>
       </WrapButton>
       <WrapButton>
-        <Button type="button" onClick={() => onLeaveFeedback('neutral')}>
+        <Button type="button" onClick={() => onLeaveFeedback(variantFeedbacks.neutral)}>
           <FaRegHandLizard />
           Neutral
         </Button>
       </WrapButton>
       <WrapButton>
-        <Button type="button" onClick={() => onLeaveFeedback('bad')}>
+        <Button type="button" onClick={() => onLeaveFeedback(variantFeedbacks.bad)}>
           <BsHandThumbsDown />
           Bad
         </Button>
